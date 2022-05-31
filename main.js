48ff33961d9f38043d943955e8d796b693146b18
Anuj Patel <103884607+code4anuj@users.noreply.github.com>
2022-05-31 16:44:34 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+    function imgSlider(anything){
+    document.querySelector('.starbucks').src = anything;
+    }
+    function changeCircleColor(color){
+        const circle = document.querySelector('.circle');
+        const circle2 = document.querySelector('.circle2');
+        circle.style.background = color;
+        circle2.style.background = color;
+    }
